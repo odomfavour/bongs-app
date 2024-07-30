53fd47d45346f16aaab1df305a5683df0d704d99
odomfavour <ogochukwuodom@gmail.com>
2024-07-30 15:45:38 +0100

    ch: fixed build

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,6 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  basePath: '/stagingv2',
-  assetPrefix: 'https://stagingv2.dpanalyticsolution.com',
 };
 
 export default nextConfig;

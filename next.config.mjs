a02bfb22561360e738b0f2df0bad04b12eadbc82
odomfavour <ogochukwuodom@gmail.com>
2024-07-30 15:27:45 +0100

    ch: fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BASEURL: 'https://bongsapi.dpanalyticsolution.com/api/v1',
-    // BASEURL: 'https://devbongsapi.dpanalyticsolution.com/api/v1',
+    // BASEURL: 'https://bongsapi.dpanalyticsolution.com/api/v1',
+    BASEURL: 'https://devbongsapi.dpanalyticsolution.com/api/v1',
   },
   images: {
     domains: ['res.cloudinary.com'],
   },
+  basePath: '/stagingv2',
+  assetPrefix: 'https://stagingv2.dpanalyticsolution.com',
 };
 
 export default nextConfig;

c3a93ab6e7676c25e906fdbfb9c71af58558c667
odomfavour <ogochukwuodom@gmail.com>
2024-08-06 13:00:26 +0100

    ch: statrted fix

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,15 @@
-import { NextResponse } from 'next/server'
-import { NextRequest } from 'next/server'
-
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    const verify = request.cookies.get("adminToken")
-    if (!verify) { 
-        return NextResponse.redirect(new URL('/login', request.url))
-    }
-
+  const verify = request.cookies.get('adminToken');
+  if (!verify) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher:[ '/dashboard']
-}
+  matcher: [],
+};

2cb8f68d706e5b5f675b23fa8f938a9dcf005495
odomfavour <ogochukwuodom@gmail.com>
2024-08-28 17:58:52 +0100

    ch: added fix

---
+++ components/procurement/CreateNewMemo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CreateNewMemo = () => {
+  return (
+    <div>
+      <p className="text-center font-semibold mb-5">Create New Memo</p>
+      <p className="text-center">
+        Please select the Bid ID for which you want to create this new memo.
+      </p>
+    </div>
+  );
+};
+
+export default CreateNewMemo;

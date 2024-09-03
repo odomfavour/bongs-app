04e3a8d93646cfcd27015b3e9e399d2c4a122b18
odomfavour <ogochukwuodom@gmail.com>
2024-09-03 16:03:17 +0100

    ch:added fixes

--- components/procurement/ApprovePurchaseOrder.tsx
+++ components/procurement/ApprovePurchaseOrder.tsx
@@ -110,6 +110,40 @@ const ApprovePurchaseOrder: React.FC<ApprovePOProps> = ({
     }
   };
 
+  const sendPurchaseOrder = async () => {
+    dispatch(toggleLoading(true));
+    try {
+      const response = await axios.post(
+        `${process.env.BASEURL}/procurement/purchase-order/${selectedPO}`,
+        {
+          id: selectedPO,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${user?.token}`,
+          },
+        }
+      );
+      console.log('Approve Response:', response);
+      if (response.status === 200) {
+        toast.success(`${response?.data?.message}`);
+      }
+      fetchPOData();
+      setOpenApprovePO(false);
+    } catch (error: any) {
+      console.error('Error:', error);
+
+      const errorMessage =
+        error?.response?.data?.message ||
+        error?.response?.data?.errors ||
+        error?.message ||
+        'Unknown error';
+      toast.error(`${errorMessage}`);
+    } finally {
+      dispatch(toggleLoading(false));
+    }
+  };
+
   return (
     <div>
       <div className="flex justify-between">
@@ -249,16 +283,16 @@ const ApprovePurchaseOrder: React.FC<ApprovePOProps> = ({
       )}
 
       {/* Conditionally render Approve and Reject buttons based on signatory status */}
-      {isSignatory && !hasSigned && (
-        <div className="flex justify-end my-5">
-          <div className="flex gap-4">
-            <button
-              className="rounded-md bg-blue-700 text-white py-2 px-4"
-              onClick={() => handleApproveOrReject('approved')}
-            >
-              Approve
-            </button>
-            <button
+      {/* {isSignatory && !hasSigned && ( */}
+      <div className="flex justify-end my-5">
+        <div className="flex gap-4">
+          <button
+            className="rounded-md bg-blue-700 text-white py-2 px-4"
+            onClick={sendPurchaseOrder}
+          >
+            Send
+          </button>
+          {/* <button
               className="rounded-md bg-red-700 text-white py-2 px-4"
               onClick={() => {
                 setIsRejecting(true);
@@ -266,10 +300,10 @@ const ApprovePurchaseOrder: React.FC<ApprovePOProps> = ({
               }}
             >
               Reject
-            </button>
-          </div>
+            </button> */}
         </div>
-      )}
+      </div>
+      {/* )} */}
     </div>
   );
 };
